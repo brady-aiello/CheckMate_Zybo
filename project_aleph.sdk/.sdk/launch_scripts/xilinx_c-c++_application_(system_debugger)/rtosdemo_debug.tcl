0557ef81948119b07d85044f42c171c3d6162280
Brady <brady.aiello@gmail.com>
2016-11-10 12:54:59 -0800

    Original Commit, performing single item GET request
    Signed-off-by: Brady <brady.aiello@gmail.com>

---
+++ project_aleph.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/rtosdemo_debug.tcl
@@ -0,0 +1,15 @@
+connect -url tcp:127.0.0.1:3121
+source C:/Users/Brady/project_aleph/project_aleph.sdk/design_1_wrapper_hw_platform_0/ps7_init.tcl
+targets -set -filter {name =~"APU" && jtag_cable_name =~ "Digilent Zybo 210279654721A"} -index 0
+rst -system
+after 3000
+targets -set -filter {jtag_cable_name =~ "Digilent Zybo 210279654721A" && level==0} -index 1
+fpga -file C:/Users/Brady/project_aleph/project_aleph.sdk/design_1_wrapper_hw_platform_0/design_1_wrapper.bit
+targets -set -filter {name =~"APU" && jtag_cable_name =~ "Digilent Zybo 210279654721A"} -index 0
+loadhw C:/Users/Brady/project_aleph/project_aleph.sdk/design_1_wrapper_hw_platform_0/system.hdf
+targets -set -filter {name =~"APU" && jtag_cable_name =~ "Digilent Zybo 210279654721A"} -index 0
+ps7_init
+ps7_post_config
+targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Digilent Zybo 210279654721A"} -index 0
+dow C:/Users/Brady/FreeRTOS_2/FreeRTOSv9.0.0/FreeRTOS/Demo/CORTEX_A9_Zynq_ZC702/RTOSDemo/Debug/RTOSDemo.elf
+bpadd -addr &main
